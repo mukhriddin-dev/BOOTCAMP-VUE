15588b1fc47c5ef67e7bcf50ee700e50aedc97e1
mukhriddin-dev <khodievm@gmail.com>
2023-02-02 10:46:13 +0500

    ok

--- module-1/lesson-9/src/script/index.js
+++ module-1/lesson-9/src/script/index.js
@@ -1,9 +1,10 @@
 "use strict";
 
 const modalWindow = document.querySelector('.modal-window'),
-      modalContent = document.querySelector('.modal-content'),
-      signUpBtn = document.querySelector('.register-btn'),
-      closeBtn = document.querySelector('.close-btn');
+    modalContent = document.querySelector('.modal-content'),
+    signUpBtn = document.querySelector('.register-btn'),
+    closeBtn = document.querySelector('.close-btn'),
+    introProduct = document.querySelector('.intro__product--info');
 
 
 signUpBtn.addEventListener('click', () => {
@@ -22,6 +23,70 @@ window.addEventListener('click', (e) => {
         modalWindow.classList.add('hidden');
     }
 })
+
+// ------------ DYNAMIC ELEMENTS ------------///
+
+
+let cards = [
+    {
+        id: 1,
+        img: './images/user1.png',
+        title: '30K',
+        description: "User order",
+    },
+    {
+        id: 2,
+        img: './images/star1.svg',
+        title: '20K',
+        description: "Reviews(4.8)",
+    },
+    {
+        id: 3,
+        img: './images/Group.png',
+        title: '300',
+        description: "Items",
+    }
+]
+
+
+
+const createElemet = (tagName, className, content) => {
+    const element = document.createElement(tagName);
+
+    if (className) {
+        element.classList.add(className);
+    }
+
+    if (content) {
+        element.innerHTML = content;
+    }
+
+    return element;
+
+}
+
+
+
+
+
+cards.forEach(items => {
+
+    const newElement = createElemet('div', "", `<div class="card hover:shadow-lg bg-white flex justify-between items-center py-5 pr-11 pl-8 rounded-sm mb-[30px]">
+
+              <span class="p-3 bg-green-100 rounded-full">
+                <img src="${items.img}" alt="user">
+              </span>
+
+              <div class="flex flex-col items-center justify-center ml-3">
+                <h2 class="text-5xl font-normal">${items.title}</h2>
+                <p>${items.description}</p>
+              </div>
+
+            </div>`)
+
+    introProduct.append(newElement);
+})
+
 
 
 
