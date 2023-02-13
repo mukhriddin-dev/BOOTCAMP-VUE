4f5cd9609d67438ebaabe103ee1de2bfaeadef0b
mukhriddin-dev <khodievm@gmail.com>
2023-02-13 12:05:15 +0500

    ok

--- module-2/lesson1/src/app.js
+++ module-2/lesson1/src/app.js
@@ -11,6 +11,11 @@ const app = Vue.createApp({
         { id: 5, title: "Info", href: "/#" },
         { id: 6, title: "Search", href: "/#" },
       ],
+      tasks: [],
+      x: 0,
+      y: 0,
+      taskName: "",
+      taskDeadline: "",
     };
   },
 
@@ -18,6 +23,29 @@ const app = Vue.createApp({
     setToggle() {
       this.toggle = !this.toggle;
       console.log(this.toggle);
+    },
+    mouseMove(e) {
+      this.x = e.clientX;
+      this.y = e.clientY;
+      console.log(this.x, this.y);
+    },
+    addNewTask() {
+      const newTask = {
+        id: Date.now(),
+        title: this.taskName,
+        date: this.taskDeadline,
+        isDone: false,
+      };
+
+      if (
+        newTask.title.trim().length === 0 ||
+        newTask.date.trim().length === 0
+      ) {
+        console.log("Please fill in the task title and date");
+      } else {
+        this.tasks.push(newTask);
+        (this.taskDeadline = ""), (this.taskName = "");
+      }
     },
   },
 
